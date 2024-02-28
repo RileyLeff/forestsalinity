83e3c1e77a8131123c48ab452d16cc24949d0014
RileyLeff <rileyleff@gmail.com>
2024-02-28 14:48:12 -0500

    jeez these csvs man

---
+++ data/processed/README.md
@@ -0,0 +1,3 @@
+# Processed Data
+
+Under absolutely no circumstances should one write to this folder or mutate anything in it manually.
--- exec/data_process/salinity.R
+++ exec/data_process/salinity.R
@@ -1,15 +1,17 @@
-porewater_19 <- read.csv(
-  file = "data/raw/forest_disturbance_plot_porewater_salinity_2019_2023.csv"
+rawdata <- lapply(
+  X = list.files("data/raw", full.names = TRUE),
+  FUN = read.csv
 )
 
-porewater_23 <- read.csv(
-  file = "data/raw/porewater_salinity_brownsville_2023.csv"
-)
 
-sosa_22 <- read.csv(
-  file = "data/raw/soil_salinity_2022_aliya.csv"
-)
 
-sosa_23 <- read.csv(
-  file = "data/raw/soil_salinity_2023_noa.csv"
+
+
+salinity <- data.frame(
+  date = NA,
+  site = NA,
+  zone = NA,
+  plot = NA,
+  subject = NA,
+  ppt = NA
 )
