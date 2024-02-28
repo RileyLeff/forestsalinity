a9f8b6ec1d2201446107641787b008b07f5995f5
RileyLeff <rileyleff@gmail.com>
2024-02-28 11:57:54 -0500

    fixed precommit again

---
+++ exec/data_process/main.R
@@ -0,0 +1,2 @@
+source("exec/data_process/salinity.R")
+source("exec/data_process/trees.R")
---
+++ exec/data_process/salinity.R
@@ -0,0 +1,15 @@
+porewater_19 <- read.csv(
+  file = "data/raw/forest_disturbance_plot_porewater_salinity_2019_2023.csv"
+)
+
+porewater_23 <- read.csv(
+  file = "data/raw/porewater_salinity_brownsville_2023.csv"
+)
+
+sosa_22 <- read.csv(
+  file = "data/raw/soil_salinity_2022_aliya.csv"
+)
+
+sosa_23 <- read.csv(
+  file = "data/raw/soil_salinity_2023_noa.csv"
+)
