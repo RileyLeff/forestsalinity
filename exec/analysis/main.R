25a92cc017be08f67a2c41dfc33d27dbe2bb2006
RileyLeff <rileyleff@gmail.com>
2024-03-01 15:52:51 -0500

    on to regression stuff

--- exec/analysis/main.R
+++ exec/analysis/main.R
@@ -1,1 +1,47 @@
 source("exec/data_process/main.R")
+
+# line 38: 2023 soil (1:5 saturation) and porewater salinity (ppt)
+#           line plot with a linear regression
+
+# keryn note says use soil salinity instead of porewater
+
+# line 67: 2022 soil sal vs 2023 soil sal, ppt
+
+# line 95: soil salinity ppt vs year
+
+# line 114: histogram brownsville porewater
+
+# line 115: boxplot ppt vs level
+
+# line 116: ggplot density of ppt by level histogramish
+
+# line 124: boxplot all plots porewater ppt
+
+# line 134: mean ppt brownsville longterm average
+
+# line 141: bville time series by zone (averaged ppt)
+
+# line 144: (ppt not averaged otherwise same)
+
+# line 161: broken?
+
+### TREES
+
+# RILEY: split spp into spp and genus columns to avoid mutating data
+# see 177 in keryn
+
+# 252: proportion dead vs salinity vs species
+
+# 266: calculate LD50 and logistic regression
+
+# 292: porewater instead of soil
+
+# 306: same ld50 and logistic regression
+
+# 350: proportion stems dead across in 2023 x soil ppt 2023
+
+# 366: broken?
+
+# 377: ggplot proportion dead vs soil salinity
+
+# 405: proportion of stems dead across in 2023 x porewater ppt
--- exec/data_process/util/salinity_helpers.R
+++ exec/data_process/util/salinity_helpers.R
@@ -1,13 +1,15 @@
 # just need clean_path() from exec/data_process/util/helpers.R
 source("exec/data_process/util/helpers.R")
 
-# need salinity conversion from data/constants.toml
+# need salinity conversion and plot/zone levels from data/constants.toml
 constants <- RcppTOML::parseToml("data/constants.toml")
 
 format_porewater_2019 <- function(df) {
+  badplotids <- which(!(df$plot %in% constants$plot_levels))
+  df <- df[-badplotids, ]
   df$date <- as.Date(df$date, format = "%m/%d/%y")
-  df$zone <- factor(gsub("\\d", "", df$plot), levels = c("H", "M", "L"), ordered = TRUE)
-  df$plot <- as.factor(df$plot)
+  df$zone <- factor(gsub("\\d", "", df$plot), levels = constants$zone_levels, ordered = TRUE)
+  df$plot <- factor(df$plot, levels = constants$plot_levels)
   df$site <- as.factor(rep("BRNV", nrow(df)))
   df$ec_ms_cm <- df$ppt / constants$ec_ms_cm_to_ppt_conversion
   df$subject <- factor(rep("porewater", nrow(df)), levels = c("porewater", "soil"))
@@ -17,10 +19,12 @@ format_porewater_2019 <- function(df) {
 }
 
 format_porewater_2023 <- function(df) {
+  badplotids <- which(!(df$plot %in% constants$plot_levels))
+  df <- df[-badplotids, ]
   df$date <- as.Date(df$date, format = "%m/%d/%y")
   df$site <- as.factor(rep("BRNV", nrow(df)))
-  df$zone <- factor(df$zone, levels = c("H", "M", "L"), ordered = TRUE)
-  df$plot <- as.factor(paste(df$zone, df$plot, sep = ""))
+  df$zone <- factor(df$zone, levels = constants$zone_levels, ordered = TRUE)
+  df$plot <- factor(paste(df$zone, df$plot, sep = ""), levels = constants$plot_levels)
   df$ppt <- df$salinity_ppt
   df$subject <- factor(rep("porewater", nrow(df)), levels = c("porewater", "soil"))
   df$ec_ms_cm <- df$ec_ms.cm
@@ -30,10 +34,12 @@ format_porewater_2023 <- function(df) {
 }
 
 format_soil_salinity_2022 <- function(df) {
+  badplotids <- which(!(df$plot %in% constants$plot_levels))
+  df <- df[-badplotids, ]
   df$date <- as.Date(df$date, format = "%m/%d/%y")
   df$site <- as.factor(rep("BRNV", nrow(df)))
-  df$zone <- factor(gsub("\\d", "", df$plot), levels = c("H", "M", "L"), ordered = TRUE)
-  df$plot <- as.factor(df$plot)
+  df$zone <- factor(gsub("\\d", "", df$plot), levels = constants$zone_levels, ordered = TRUE)
+  df$plot <- factor(df$plot, levels = constants$plot_levels)
   df$subject <- factor(rep("soil", nrow(df)), levels = c("porewater", "soil"))
   df$ec_ms_cm <- df$ec_ms.cm
   df$ppt <- constants$ec_ms_cm_to_ppt_conversion * df$ec_ms_cm
@@ -43,9 +49,11 @@ format_soil_salinity_2022 <- function(df) {
 }
 
 format_soil_salinity_2023 <- function(df) {
+  badplotids <- which(!(df$plot %in% constants$plot_levels))
+  df <- df[-badplotids, ]
   df$date <- as.Date(df$date, format = "%m/%d/%y")
-  df$zone <- factor(df$zone, levels = c("H", "M", "L"), ordered = TRUE)
-  df$plot <- as.factor(df$plot)
+  df$zone <- factor(df$zone, levels = constants$zone_levels, ordered = TRUE)
+  df$plot <- factor(df$plot, levels = constants$plot_levels)
   df$site <- as.factor(rep("BRNV", nrow(df)))
   df$subject <- factor(rep("soil", nrow(df)), levels = c("porewater", "soil"))
   df$ec_ms_cm <- df$ec_us.cm / 1000
