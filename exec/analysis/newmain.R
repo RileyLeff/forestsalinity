ca55146d91156257db0faf93be0ac2ece61880cd
RileyLeff <rileyleff@gmail.com>
2024-03-01 12:00:58 -0500

    fixed ec/ppt

--- exec/analysis/newmain.R
+++
@@ -1,1 +0,0 @@
-# will eventually delete old main.R and rename this file to main.R
--- exec/data_process/util/salinity_helpers.R
+++ exec/data_process/util/salinity_helpers.R
@@ -1,12 +1,15 @@
 # just need clean_path() from exec/data_process/util/helpers.R
 source("exec/data_process/util/helpers.R")
 
+# need salinity conversion from data/constants.toml
+constants <- RcppTOML::parseToml("data/constants.toml")
+
 format_porewater_2019 <- function(df) {
   df$date <- as.Date(df$date, format = "%m/%d/%y")
   df$zone <- factor(gsub("\\d", "", df$plot), levels = c("H", "M", "L"), ordered = TRUE)
   df$plot <- as.factor(df$plot)
   df$site <- as.factor(rep("BRNV", nrow(df)))
-  df$ec_ms_cm <- NA
+  df$ec_ms_cm <- df$ppt / constants$ec_ms_cm_to_ppt_conversion
   df$subject <- factor(rep("porewater", nrow(df)), levels = c("porewater", "soil"))
   cols_to_keep <- c("date", "site", "zone", "plot", "subject", "ppt", "ec_ms_cm")
   df <- df[, cols_to_keep]
@@ -32,8 +35,8 @@ format_soil_salinity_2022 <- function(df) {
   df$zone <- factor(gsub("\\d", "", df$plot), levels = c("H", "M", "L"), ordered = TRUE)
   df$plot <- as.factor(df$plot)
   df$subject <- factor(rep("soil", nrow(df)), levels = c("porewater", "soil"))
-  df$ppt <- NA
   df$ec_ms_cm <- df$ec_ms.cm
+  df$ppt <- constants$ec_ms_cm_to_ppt_conversion * df$ec_ms_cm
   cols_to_keep <- c("date", "site", "zone", "plot", "subject", "ppt", "ec_ms_cm")
   df <- df[, cols_to_keep]
   return(df)
@@ -45,8 +48,8 @@ format_soil_salinity_2023 <- function(df) {
   df$plot <- as.factor(df$plot)
   df$site <- as.factor(rep("BRNV", nrow(df)))
   df$subject <- factor(rep("soil", nrow(df)), levels = c("porewater", "soil"))
-  df$ppt <- NA
   df$ec_ms_cm <- df$ec_us.cm / 1000
+  df$ppt <- constants$ec_ms_cm_to_ppt_conversion * df$ec_ms_cm
   cols_to_keep <- c("date", "site", "zone", "plot", "subject", "ppt", "ec_ms_cm")
   df <- df[, cols_to_keep]
   return(df)
