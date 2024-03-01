b9976fd417338f6f79559b8bb9d8081315e65f2c
RileyLeff <rileyleff@gmail.com>
2024-03-01 09:43:00 -0500

    functional programming cookup

--- exec/data_process/salinity.R
+++ exec/data_process/salinity.R
@@ -1,19 +1,22 @@
+# adds the utility functions clean_path() and apply_data_formatter_by_name()
 source("exec/data_process/util/helpers.R")
 
-# adds the utility functions to format each dataset to the global environment
+# adds utility functions to format each dataset to the global environment
 # also adds a map that pairs each dataset with its corresponding formatter function to the env
+# this is called salinity_data_format_map
 source("exec/data_process/util/salinity_helpers.R")
 
 # read salinity datasets into a list and name them by path
 salinity_paths <- list.files("data/raw/salinity", full.names = TRUE)
 raw_salinity_data <- lapply(salinity_paths, read.csv)
-names(raw_salinity_data) <- clean_path(salinity_paths)
+names(raw_salinity_data) <- clean_path(salinity_paths) # from exec/data_process/util/helpers.R
 
-salinity <- data.frame(
-  date = NA,
-  site = NA,
-  zone = NA,
-  plot = NA,
-  subject = NA,
-  ppt = NA
+salinity <- merge(
+  # function from exec/data_process/util/helpers.R
+  apply_data_formatter_by_name(
+    # object from exec/data_process/util/salinity_helpers.R
+    format_map = salinity_data_format_map, 
+    data_list = raw_salinity_data
+  )
+  all = TRUE
 )
--- exec/data_process/util/helpers.R
+++ exec/data_process/util/helpers.R
@@ -1,7 +1,41 @@
+# example usage:
+# clean_path("my/crazy/path/to/a/thing.jpeg")
+#
+# Output:
+# chr
+# [1] "thing"
 clean_path <- function(path) {
   tools::file_path_sans_ext(
     basename(
       path
     )
+  )
+}
+
+# example usage:
+#
+# fl = list(one = function(x){x+5}, two = function(x){x*5}, three = function(x){x/5})
+# dl = list(one = c(1,2,3), two = c(4,5,6), three = c(7,8,9))
+#
+# apply_data_formatter_by_name(
+#   format_map = fl,
+#   data_list = dl
+# )
+#
+# Output:
+# List
+# $one
+# [1] 6 7 8
+# $two
+# [1] 20 25 30
+# $three
+# [1] 1.4 1.6 1.8
+#
+# we use names instead of position so things can be out of order, reorganized, etc in future
+# if more datasets are added.
+apply_data_formatter_by_name <- function(format_map, data_list) {
+  Map(
+    function(fn, dn) format_map[[fn]](data_list[[dn]]),
+    names(format_map), names(data_list)
   )
 }
