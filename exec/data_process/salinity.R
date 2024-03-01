39fa93c0013fbc2caeca28a65cfd801e9e915b17
RileyLeff <rileyleff@gmail.com>
2024-03-01 09:05:49 -0500

    data function maps

--- exec/data_process/salinity.R
+++ exec/data_process/salinity.R
@@ -1,11 +1,11 @@
-raw_data <- lapply(
-  X = list.files("data/raw", full.names = TRUE),
-  FUN = read.csv
-)
+# adds the utility functions to format each dataset to the global environment
+# also adds a map that pairs each dataset with its corresponding formatter function to the env
+source("exec/data_process/util/salinity_helpers")
 
-
-
-
+# read salinity datasets into a list and name them
+salinity_paths <- list.files("data/raw/salinity", full.names = TRUE)
+raw_salinity_data <- lapply(salinity_paths, read.csv)
+names(raw_salinity_data) <- basename(salinity_paths)
 
 salinity <- data.frame(
   date = NA,
---
+++ exec/data_process/util/salinity_helpers.R
@@ -0,0 +1,28 @@
+format_porewater_2019 <- function(data) {
+  data + 1
+}
+
+format_porewater_2023 <- function(data) {
+  data + 2
+}
+
+format_soil_salinity_2022 <- function(data) {
+
+}
+
+format_soil_salinity_2023 <- function(data) {
+
+}
+
+salinity_data_format_map <- list(
+  format_porewater_2019,
+  format_porewater_2023,
+  format_soil_salinity_2022,
+  format_soil_salinity_2023
+)
+
+names(salinity_data_format_map) <- tools::file_path_sans_ext(
+  basename(
+    list.files("data/raw/salinity")
+  )
+)
