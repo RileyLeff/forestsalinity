cf2e36d72c67eb7f18d8d9c16ab2b8e37d0e9050
RileyLeff <rileyleff@gmail.com>
2024-03-01 11:40:11 -0500

    tree_census readin

--- exec/data_process/trees.R
+++ exec/data_process/trees.R
@@ -1,1 +1,39 @@
-tree_census_raw <- read.csv("data/raw/forest_census/trees_2023.csv")
+tree_census <- read.csv(
+  file = "data/raw/forest_census/trees_2023.csv",
+  na.strings = c("NA", "na", "NA ")
+)
+
+tree_census$zone <- factor(tree_census$level, levels = c("H", "M", "L"), ordered = TRUE)
+
+# !!! NOTE: completely arbitrary decision to fill in missing dates with the middle of the month
+tree_census$day[which(is.na(tree_census$day))] <- 15
+
+# !!! NOTE: completely arbitrary decision to fill in missing months with the middle of the year
+tree_census$month[which(is.na(tree_census$month))] <- 6
+
+tree_census$date <- as.Date(
+  paste(
+    tree_census$year,
+    tree_census$month,
+    tree_census$day,
+    sep = "/"
+  )
+)
+
+tree_census$alive <- as.logical(tree_census$alive)
+tree_census$standing <- as.logical(tree_census$standing)
+
+tree_census$plot <- as.factor(tree_census$plot)
+tree_census$tree_number <- as.factor(tree_census$tree_number)
+tree_census$tree_code <- as.factor(tree_census$tree_code)
+tree_census$trunk_number <- as.factor(tree_census$trunk_number)
+tree_census$spp <- as.factor(tree_census$spp)
+
+tree_census$site <- as.factor(rep("BRNV", nrow(tree_census)))
+
+cols_to_keep <- c(
+  "date", "site", "zone", "plot", "tree_number", "tree_code", "trunk_number", "spp",
+  "dbh_mm", "health", "alive", "old_tag", "standing", "notes"
+)
+
+tree_census <- tree_census[, cols_to_keep]
