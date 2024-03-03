73d566998b1cd521a5f711de8352f05800ded702
RileyLeff <rileyleff@gmail.com>
2024-03-03 10:07:57 -0500

    cleaned up miscategorized mortalities

--- exec/data_process/trees.R
+++ exec/data_process/trees.R
@@ -1,3 +1,5 @@
+source("exec/data_process/util/trees_helpers.R")
+
 RcppTOML::parseToml("data/constants.toml")
 
 tree_census <- read.csv(
@@ -27,14 +29,23 @@ tree_census$standing <- as.logical(tree_census$standing)
 
 tree_census$plot <- factor(tree_census$plot, levels = constants$plot_levels)
 
+tree_census$spp[which(tree_census$spp == "Unidentifiable")] <- "Unidentifiable spp."
+split_spp <- strsplit(tree_census$spp, split = " ")
+tree_census$genus <- sapply(split_spp, `[`, 1)
+tree_census$spp <- sapply(split_spp, `[`, 2)
+
 convert_to_factor <- c("tree_number", "tree_code", "trunk_number", "spp")
 for (column in convert_to_factor) tree_census[, column] <- as.factor(tree_census[, column])
 
 tree_census$site <- as.factor(rep("BRNV", nrow(tree_census)))
 
 cols_to_keep <- c(
-  "date", "site", "zone", "plot", "tree_number", "tree_code", "trunk_number", "spp",
+  "date", "site", "zone", "plot", "tree_number", "tree_code", "trunk_number", "genus", "spp",
   "dbh_mm", "health", "alive", "old_tag", "standing", "notes"
 )
 
 tree_census <- tree_census[, cols_to_keep]
+
+tree_census <- cleanup_alive_vs_health(tree_census)
+
+check_alive_vs_health(tree_census)
