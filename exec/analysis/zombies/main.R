62449ae4bd5278d44ec996e60ff443bbfa87003d
RileyLeff <rileyleff@gmail.com>
2024-03-03 12:22:22 -0500

    added zombie analysis

---
+++ exec/analysis/zombies/main.R
@@ -0,0 +1,4 @@
+source("exec/data_process/main.R")
+source("exec/analysis/zombies/util.R")
+
+zombie_tree_codes <- get_zombie_tree_codes(df = tree_census, trunk_num = 1)
---
+++ exec/analysis/zombies/util.R
@@ -0,0 +1,50 @@
+library(dplyr)
+
+# returns a vector of tree IDs that demonstrate zombification at any point in the survey
+get_zombie_tree_codes <- function(df, trunk_num = 1) {
+  statuses <- get_status_by_tree_code(df, trunk_num)
+  names(statuses[which(statuses == "zombie")])
+}
+
+# returns a list that maps tree_code as a name to one of "alive", "dead non-zombie", or "zombie" as a value.
+# learning rust completely changed the way I think about programming in other languages
+# i.e. this would return an enum called TreeStatus in rust
+get_status_by_tree_code <- function(df, trunk_num = 1) {
+  filtered_df <- as.data.frame(
+    df %>%
+      filter(trunk_number == trunk_num) %>%
+      filter(!is.na(alive)) %>%
+      arrange(date)
+  )
+  status_by_code_map <- .internal_status_finder(
+    split(
+      x = filtered_df,
+      f = filtered_df$tree_code
+    )
+  )
+}
+
+# don't call this directly, it will be get confused by trunk_number
+.internal_status_finder <- function(df) {
+  lapply(
+    X = df,
+    FUN = function(x) {
+      # find instances logged as alive = FALSE
+      dead_inds <- which(!x$alive)
+
+      # if there are none, it's still alive
+      if (length(dead_inds) == 0) {
+        return("alive")
+
+        # else there's at least one instance of dead
+      } else {
+        # if all rows after first appearance of alive = FALSE are also marked alive = FALSE
+        if (identical(dead_inds, dead_inds[1]:nrow(x))) {
+          return("dead non-zombie")
+        } else {
+          return("zombie")
+        }
+      }
+    }
+  )
+}
